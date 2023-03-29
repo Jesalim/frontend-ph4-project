670139cbb00510a9de7b7861b6c31696e58ca2e4
Marylucy123450 <marylucyatienoomenda@gmail.com>
2023-03-29 10:30:35 +0300

    debuging Main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import Home from '../home/Home'
-import Explore from '../explore/Explore'
-import Book from '../book/Book'
-import WhyUs from '../whyUs/WhyUs'
-import About from '../about/About'
+
 import Footer from '../Footer'
 
 function Main() {
