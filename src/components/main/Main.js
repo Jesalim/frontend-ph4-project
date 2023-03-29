35ba29d719142c60bf4a8485f053a0732f4476fd
Marylucy123450 <marylucyatienoomenda@gmail.com>
2023-03-29 10:31:26 +0300

    fixed some error

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import Home from '../home/Home'
+import Explore from '../explore/Explore'
+import Book from '../book/Book'
+import WhyUs from '../whyUs/WhyUs'
+import About from '../about/About'
 import Footer from '../Footer'
 
 function Main() {
