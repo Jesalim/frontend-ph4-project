784b6042624c16f26cfbba39c9d1c040e05386a6
Marylucy123450 <marylucyatienoomenda@gmail.com>
2023-03-30 16:55:31 +0300

    updated changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/navbar/NavBar'
+import Main from './components/main/Main';
+import Ticket from './components/ticket/Ticket'
+import { Route, Routes } from 'react-router-dom';
+ import Login from './components/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* Uncomment the Login part to finish up with Authentications */}
+    
+     { <Login /> }
+     <NavBar />
+     <section className='sections'>
+       <Routes>
+          <Route path='/' element={<Main />}  />
+          <Route path='/ticket' element={<Ticket />} />
+       </Routes>
+     </section>
+    </>
   );
 }
 
