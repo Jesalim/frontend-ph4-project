320bf2d06839927b1a1f54c19d68520e91d86420
kidhri salim <salimkhidhri@gmail.com>
2023-04-03 06:57:10 +0300

    importing

---
+++ src/components/whyUs/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react'
+//import { Col, Container, Row } from 'react-bootstrap'
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import "./footer.css";
