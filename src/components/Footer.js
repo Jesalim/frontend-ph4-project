83e9fc2ae8879a0ac2c925fbed88c847f7f01aea
Jesalim <jessyculture@gmail.com>
2023-04-03 21:52:46 +0300

    packages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
 //import { Col, Container, Row } from 'react-bootstrap'
 // import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import FacebookIcon from "@material-ui/icons/Facebook";
+// import TwitterIcon from "@material-ui/icons/Twitter";
+// import FacebookIcon from "@material-ui/icons/Facebook";
 import "./footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-       <TwitterIcon /> <FacebookIcon />
+       
       </div>
       <p> &copy; 2023 Flightbooking.com</p>
     </div>
